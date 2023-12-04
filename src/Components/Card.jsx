1d620255a3a257f7c4a23a989b77f6473cece2ad
Tulza <40633228+tulza@users.noreply.github.com>
2023-12-05 01:59:53 +1300

    card component added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,23 @@
+import clsx from "clsx";
 import React from "react";
+import PropTypes from "prop-types";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ label, description, color }) => {
+  return (
+    <div className="bg-white rounded-[0.5rem] overflow-hidden shadow">
+      <div className={clsx(`${color} h-1`)} />
+      <div className="p-10">
+        <p>{label}</p>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  label: PropTypes.string,
+  description: PropTypes.string,
+  color: PropTypes.string,
 };
 
 export default Card;
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,12 +1,29 @@
 import React, { Fragment } from "react";
+import Card from "./Card";
 
 const Homepage = () => {
   return (
     <Fragment>
-      <div className="bg-white rounded-[0.5rem]">
-        <div className="bg-Red h-1" />
-        sadasd
-      </div>
+      <Card
+        label="Team Builder"
+        description="Scans our talent network to create the optimal team for your project"
+        color="bg-Red"
+      />
+      <Card
+        label="Team Builder"
+        description="Scans our talent network to create the optimal team for your project"
+        color="bg-Cyan"
+      />
+      <Card
+        label="Team Builder"
+        description="Scans our talent network to create the optimal team for your project"
+        color="bg-Orange"
+      />
+      <Card
+        label="Team Builder"
+        description="Scans our talent network to create the optimal team for your project"
+        color="bg-Blue"
+      />
     </Fragment>
   );
 };
