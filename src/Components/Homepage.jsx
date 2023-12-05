eb93b16eb0f90657b5b125ace875386c89515872
Tulza <40633228+tulza@users.noreply.github.com>
2023-12-06 06:39:16 +1300

    page intro added

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
 import Card from "./Card";
 import CardsBox from "./CardsBox.jsx";
-import Introduction from "../../Introduction.jsx";
+import Introduction from "./Introduction.jsx";
 
 const Homepage = () => {
   return (
     <Fragment>
-      <div>
+      <div className="grid">
         <Introduction />
         <CardsBox />
       </div>
---
+++ src/Components/Introduction.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Introduction = () => {
+  return (
+    <div className="w-[500px] justify-self-center text-center">
+      <p className="font-extralight text-[40px]">
+        Reliable, efficient delivery
+      </p>
+      <p className="font-semibold text-[40px]"> Powered by Technology</p>
+      <p className="my-[1rem] mb-[2rem]">
+        Our Artificial Intelligence powered tools use millions of project data
+        points to ensure that your project is successful
+      </p>
+    </div>
+  );
+};
+
+export default Introduction;
